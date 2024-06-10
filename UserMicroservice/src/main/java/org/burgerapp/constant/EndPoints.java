a6524fcf40d288d20c306a100ae609e6d6cb74dd
ertugrulsaliher <ertugrulsaliher@gmail.com>
2024-06-10 13:50:40 +0300

    Salih 10.06.2024 13:50

---
+++ UserMicroservice/src/main/java/org/burgerapp/constant/EndPoints.java
@@ -0,0 +1,24 @@
+package org.burgerapp.constant;
+
+public class EndPoints {
+
+    public static final String VERSION = "/v1";
+
+    public static final String API = "/api";
+
+
+    public static final String ROOT=VERSION+API;
+
+    public static final String USER_PROFILE = ROOT+"/userProfile";
+
+
+
+    public static final String SAVE = "/save";
+    public static final String REGISTER = "/register";
+    public static final String DELETE = "/delete";
+    public static final String VERIFY_ACCOUNT = "/verifyAccount";
+    public static final String LOGIN = "/login";
+    public static final String FIND_ALL = "/findAll";
+
+
+}
