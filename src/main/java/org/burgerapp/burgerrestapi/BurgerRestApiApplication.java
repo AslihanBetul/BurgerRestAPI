cbf2f3f241adb1ec380992ed82507ac785b447e7
ertugrulsaliher <ertugrulsaliher@gmail.com>
2024-06-10 11:03:56 +0300

    Initial commit

---
+++ src/main/java/org/burgerapp/burgerrestapi/BurgerRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.burgerapp.burgerrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerRestApiApplication.class, args);
+    }
+
+}
